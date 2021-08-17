2932c9ef527a38859a057321b91fc75fc3f23ebf
Anjana <anjanapknr@gmail.com>
2021-08-17 18:51:13 -0400

    add state,view state

--- src/custom_screens/view_state.js
+++ src/custom_screens/view_state.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Form, Button, Container, Row, Col,Table} from "react-bootstrap";
 import firebase from 'firebase/app';
 import 'firebase/firebase-firestore';
 import 'firebase/firebase-auth';
@@ -7,88 +7,87 @@ import { Redirect } from 'react-router-dom';
 
 export class ViewStatePage extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            name: '',
-            date_of_birth: '',
-            address: '',
-            house_name: '',
-            place: '',
-            post: '',
-            pincode: '',
-            district: '',
-            state: '',
-            loggedin: false
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this)
-
-
-
-
-    }
-    handleChange(event) {
-        let data = {};
-        data[event.target.name] = event.target.value;
-        this.setState(data);
-    }
-    async handleSubmit(event) {
+  constructor(props){
+            super(props);
+            this.state={
+              rows:[]
+            }
+        this.getStates=this.getStates.bind(this);
+        this.update=this.update.bind(this);
+      };
+    
+     async getStates()
+      {
         let firestore = firebase.firestore();
-
-        event.preventDefault();
-        let auth = firebase.auth();
-
-        try {
-            await auth.createUserWithEmailAndPassword(this.state.email, this.state.password);
-            await firestore.collection("registration").add({ name: this.state.name, date_of_birth: this.state.date_of_birth, address: this.state.address, house_name: this.state.house_name, place: this.state.place, post: this.state.post, pincode: this.state.pincode, district: this.state.district, state: this.state.state });
-            alert("registred")
-            this.setstate({ loggedin: true })
-        }
-        catch (e) {
-            alert(e.message);
-        }
-    }
-    redirect() {
-        return <Redirect to='/search' />;
-    }
-    render() {
-        return (
+        let states = await firestore.collection("states").get()
+        let rows=[];
+        states.forEach((state)=>{
+            rows.push(<tr>
+              
+                <td>
+                  {
+                    state.data()["state"]
+                  }
+        </td>
+               
+                 <td>
+                   <Button onClick={(e)=>{this.update(state.id)}}>update</Button>
+                   </td>
+                   <td>
+                     <Button onClick={(e)=>{this.deleteData(state.id)}}>delete</Button>
+                   </td>
+            </tr>)
+                
+        });
+      
+      this.setState({rows:rows})
+      }
+      render()
+      {
+        return(
             <>
-
-                {this.state.loggedin ? this.redirect() : this.getContent()}
-            </>
+         <h2>
+            VIEW STATES
+        </h2>
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>STATE</th>
+            
+            
+    
+            <th>ACTION1</th>
+            <th>ACTION2</th>
+          </tr>
+        </thead>
+        <tbody>
+            {
+              this.state.rows
+            }
+        </tbody>
+        </Table>
+        </>
         );
     }
-    getContent() {
-        return (
-            <>
-                <Container>
-                    <Row className="d-flex justify-content-center align-items-center " >
-                        <Col lg={6}>
-
-                            <Form id="form" onSubmit={this.handleSubmit}>
-                                <Form.Group>
-                                    <Form.Label>
-                                        State
-                                    </Form.Label>
-                                    <Form.Control type="text" name="email" value={this.state.email} onChange={this.handleChange} className="form-control" />
-                                </Form.Group>
-
-                                <Form.Group>
-
-
-                                    <Button type="submit" className="btn btn-primary"> VIEW</Button>
-                                </Form.Group>
-
-                            </Form>
-                        </Col>
-                    </Row>
-                </Container>
-            </>
-
-        );
-    }
+       componentDidMount()
+       {
+         this.getStates()
+       }
+       update(id)
+       {
+         this.props.history.push({
+           pathname:"/edit_states",
+           state:{
+             id:id
+           }
+         })
+       }
+       async deleteData(id){
+        if(window.confirm("Are you sure?")){
+          let firestore=firebase.firestore();
+          await firestore.collection("states").doc(id).delete();
+          alert("Deleted");
+          this.getStates();
+        }
+      }
 }
